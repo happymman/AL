4387737d41a3274a2d8c2ab1583fdd741c54591d
heenamkang <abc_always@naver.com>
2023-03-20 15:02:06 +0900

    p43163

---
+++ Solution/src/BFS_DFS/P43163_단여변환.java
@@ -0,0 +1,52 @@
+package BFS_DFS;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+class P43163_단여변환 {
+    private static class State{
+        String word;
+        Integer step;
+
+        public State(String word, Integer step){
+            this.word = word;
+            this.step = step;
+        }
+    }
+
+    private boolean isConvertable(String src, String dst){
+        char[] srcArr = src.toCharArray();
+        char[] dstArr = dst.toCharArray();
+
+        int diff=0;
+        for(int i=0 ; i<srcArr.length; i++){
+            if(srcArr[i] != dstArr[i]) diff++;
+        }
+        return diff == 1;
+    }
+
+
+    public int solution(String begin, String target, String[] words) {
+        boolean[] isVisited = new boolean[words.length];
+
+        Queue<State> queue = new LinkedList<>();
+        queue.add(new State(begin, 0));
+
+        while(!queue.isEmpty()){
+            State state = queue.poll();
+
+            if(state.word.equals(target))
+                return state.step;
+            for(int i=0;i<words.length;i++){
+                String next = words[i];
+
+                if(isVisited[i]) continue;
+                if(!isConvertable(state.word, next)) continue;
+                isVisited[i] = true;
+                queue.add(new State(next, state.step+1));
+            }
+        }
+        return 0;
+
+    }
+}
