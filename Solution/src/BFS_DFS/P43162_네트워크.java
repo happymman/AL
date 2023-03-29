496f5e3cc7256bc241384d61831c1d62d19b4544
heenamkang <abc_always@naver.com>
2023-03-29 21:57:25 +0900

    p43162_3

--- Solution/src/BFS_DFS/P43162_네트워크.java
+++ Solution/src/BFS_DFS/P43162_네트워크.java
@@ -34,6 +34,7 @@ public class P43162_네트워크 {
     }
 }
 
+//2번째 풀이
 //import java.util.Stack;
 //
 //public class Solution{
@@ -65,6 +66,61 @@ public class P43162_네트워크 {
 //            networkCount++;
 //        }
 //        return networkCount;
+//    }
+//}
+
+//3번째 풀이
+//import java.util.Queue;
+//        import java.util.LinkedList;
+//public class Solution{
+//    public class State{
+//        int x;
+//        int y;
+//        int step;
+//
+//        public State(int x, int y, int step){
+//            this.x=x;
+//            this.y=y;
+//            this.step=step;
+//        }
+//    }
+//
+//    private static final int[] dx = {0,1,0,-1};
+//    private static final int[] dy = {-1,0,1,0};
+//
+//    public int solution(int[][] maps){
+//        //방문배열
+//        boolean[][] isVisited = new boolean[maps.length][maps[0].length];
+//        //초기설정
+//        Queue<State> q = new LinkedList<>();
+//        isVisited[0][0] = true;
+//        q.add(new State(0,0,1));
+//
+//        while(!q.isEmpty()){
+//            //pop
+//            State state = q.poll();
+//
+//            //target검사
+//            if(state.y == maps.length-1 && state.x == maps[0].length-1) return state.step;
+//
+//            for(int i=0;i<4;i++){
+//                int nx = state.x+dx[i];
+//                int ny = state.y+dy[i];
+//
+//                //테두리
+//                if(ny <0 || ny>maps.length-1 ||nx<0 || nx>maps[0].length-1) continue;
+//                //내부 전이가능검사
+//                if(maps[ny][nx] ==0) continue;
+//
+//                //방문검사
+//                if(isVisited[ny][nx]) continue;
+//                isVisited[ny][nx] = true;
+//
+//                //전이
+//                q.add(new State(nx, ny, state.step+1));
+//            }
+//        }
+//        return -1;
 //    }
 //}
 
