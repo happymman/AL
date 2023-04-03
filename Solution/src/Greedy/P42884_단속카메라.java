43a06f3031a430d2360a49e49095d2723cbc682a
heenamkang <abc_always@naver.com>
2023-04-03 13:40:18 +0900

    P42884

---
+++ Solution/src/Greedy/P42884_단속카메라.java
@@ -0,0 +1,23 @@
+package Greedy;
+
+import java.util.Arrays;
+        import java.util.Comparator;
+
+class P42884_단속카메라 {
+    public int solution(int[][] routes) {
+        Arrays.sort(routes, Comparator.comparingInt(route->route[1]));
+        boolean[] isVisited = new boolean[routes.length];
+
+        int camera =0;
+        for(int i=0;i<routes.length;i++){
+            if(isVisited[i]) continue;
+            isVisited[i] = true;
+
+            for(int j=0 ; j<routes.length;j++){
+                if(routes[j][0]<=routes[i][1]) isVisited[j] = true;
+            }
+            camera++;
+        }
+        return camera;
+    }
+}
