50ffaeab4a46220679cb6a3bbfe67aa27f470820
heenamkang <abc_always@naver.com>
2023-04-07 20:30:46 +0900

    P42839

---
+++ Solution/src/완전탐색/P42839_소수찾기.java
@@ -0,0 +1,44 @@
+package 완전탐색;
+
+import java.util.HashSet;
+
+public class P42839_소수찾기{
+    private static char[] arr;
+    private static boolean[] isVisited;
+    private static HashSet<Integer> set = new HashSet<>();
+
+    public int solution(String numbers){
+        arr = numbers.toCharArray();
+        isVisited = new boolean[numbers.length()];
+
+        recursion("", 0);
+
+        return set.size();
+
+    }
+
+    private void recursion(String str, int size){ //"", 0
+        if(str!=""){
+            int num = Integer.parseInt(str);
+            if(isPrime(num)) set.add(num);
+        }
+
+        if(size == arr.length) return;
+
+        for(int i=0;i<arr.length;i++){
+            if(isVisited[i]) continue;
+            isVisited[i] = true;
+
+            recursion(str+arr[i], size+1);
+            isVisited[i] = false;
+        }
+    }
+
+    private boolean isPrime(int num){
+        if(num<2) return false;
+        for(int i=2;i<=Math.sqrt(num);i++){
+            if(num%i==0) return false;
+        }
+        return true;
+    }
+}
