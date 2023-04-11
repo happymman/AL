a31638254658f185bad2c8ad8180c311c794d9f4
heenamkang <abc_always@naver.com>
2023-04-11 11:40:49 +0900

    P43163_3

--- Solution/src/BFS_DFS/P43163_단어변환.java
+++ Solution/src/BFS_DFS/P43163_단어변환.java
@@ -3,7 +3,7 @@ package BFS_DFS;
 import java.util.LinkedList;
 import java.util.Queue;
 
-class P43163_단여변환 {
+class P43163_단어변환 {
     private static class State{
         String word;
         Integer step;
@@ -50,3 +50,46 @@ class P43163_단여변환 {
 
     }
 }
+
+//import java.util.Queue;
+//        import java.util.LinkedList;
+//
+//public class Solution{
+//    private static boolean isConvertible(String src, String dst){
+//        int diff=0;
+//        for(int i=0;i<src.length();i++){
+//            if(src.charAt(i) != dst.charAt(i)) diff++;
+//        }
+//        return diff ==1;
+//    };
+//
+//    private class State{
+//        String word;
+//        int step;
+//        public State(String word, int step){
+//            this.word = word;
+//            this.step = step;
+//        }
+//    }
+//
+//    public int solution(String begin, String target, String[] words){
+//        boolean[] isVisited = new boolean[words.length];
+//        Queue<State> q = new LinkedList<>();
+//        q.add(new State(begin, 0));
+//
+//        while(!q.isEmpty()){
+//            State state = q.poll();
+//
+//            if(state.word.equals(target)) return state.step;
+//
+//            for(int i=0;i<words.length;i++){
+//                if(isVisited[i]) continue;
+//                if(!isConvertible(state.word, words[i])) continue;
+//                isVisited[i] = true;
+//
+//                q.add(new State(words[i], state.step+1));
+//            }
+//        }
+//        return 0;
+//    }
+//}
