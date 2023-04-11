bbcf9e4bcf7c06fda4876b603a7073df5a7b32e0
heenamkang <abc_always@naver.com>
2023-04-11 11:14:44 +0900

    P1844

--- Solution/src/BFS_DFS/P1844_게임맵최단거리.java
+++ Solution/src/BFS_DFS/P1844_게임맵최단거리.java
@@ -50,3 +50,52 @@ class P1844_게임맵최단거리 {
         return -1;
     }
 }
+
+//import java.util.Queue;
+//        import java.util.LinkedList;
+//
+//public class Solution{
+//    private class State{
+//        int x;
+//        int y;
+//        int distance;
+//
+//        public State(int x, int y, int distance){
+//            this.x = x;
+//            this.y = y;
+//            this.distance = distance;
+//        }
+//    }
+//
+//    private static int[] dx = {0,1,0,-1};
+//    private static int[] dy = {-1,0,1,0};
+//
+//    public int solution(int[][] maps){
+//        boolean[][] isVisited = new boolean[maps.length][maps[0].length];
+//
+//        Queue<State> q = new LinkedList<>();
+//        q.add(new State(0,0,1));
+//
+//        while(!q.isEmpty()){
+//            State state = q.poll();
+//
+//            if(state.x == maps[0].length-1 && state.y == maps.length-1){
+//                return state.distance;
+//            }
+//
+//            for(int i=0;i<4;i++){
+//                int ny = state.y+dy[i];
+//                int nx = state.x+dx[i];
+//
+//                if(ny<0 || ny >= maps.length || nx<0 || nx>=maps[0].length) continue;
+//                if(maps[ny][nx] == 0) continue;
+//
+//                if(isVisited[ny][nx]) continue;
+//                isVisited[ny][nx] = true;
+//
+//                q.add(new State(nx, ny, state.distance+1));
+//            }
+//        }
+//        return -1;
+//    }
+//}
