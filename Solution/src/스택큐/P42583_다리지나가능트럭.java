acb64466112eaa70899e0c1d899d4348b10a276e
heenamkang <abc_always@naver.com>
2023-04-13 21:46:12 +0900

    P42583_다리지나가는트럭

---
+++ Solution/src/스택큐/P42583_다리지나가능트럭.java
@@ -0,0 +1,38 @@
+package 스택큐;
+
+
+import java.util.*;
+class P42583_다리지나가능트럭 {
+    public int solution(int bridge_length, int weight, int[] truck_weights) {
+        Queue<Integer> bridge = new LinkedList<>();
+        int bridgeWeight = 0;
+
+        for(int i=0;i<bridge_length;i++){
+            bridge.add(0);
+        }
+
+        int time =0;
+        int truckIndex = 0;
+
+        while(truckIndex < truck_weights.length){
+            bridgeWeight -= bridge.poll();
+
+            int truckWeight = truck_weights[truckIndex];
+            if(bridgeWeight+truckWeight<=weight){
+                bridge.add(truckWeight);
+                bridgeWeight+=truckWeight;
+                truckIndex++;
+            }else{
+                bridge.add(0);
+            }
+            time++;
+        }
+
+        while(bridgeWeight>0){
+            bridgeWeight -= bridge.poll();
+            time++;
+        }
+
+        return time;
+    }
+}
