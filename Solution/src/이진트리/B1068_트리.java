f6c22ef1e015b47a57f58384587f93c54b5f6a63
heenamkang <abc_always@naver.com>
2023-05-11 21:39:04 +0900

    B1068_트리

--- Solution/src/이진트리/B1068_트리.java
+++ Solution/src/이진트리/B1068_트리.java
@@ -4,9 +4,9 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class B1068_트리 {
-    private static int leafNode=0;
-    private static int M;
-    private static ArrayList<Integer> tree[];
+    private static int answer =0;
+    private static int deleteNode=0;
+    private static ArrayList<Integer>[] tree;
     private static boolean[] isVisited;
 
     public static void main(String[] args) {
@@ -15,37 +15,44 @@ public class B1068_트리 {
 
         tree = new ArrayList[N];
         isVisited = new boolean[N];
-        isVisited[0] = true;
 
         for(int i=0;i<N;i++){
             tree[i] = new ArrayList<>();
         }
 
+        int root =0;
         for(int i=0;i<N;i++){
-            int parent = sc.nextInt();
-            if(parent != -1)
-                tree[parent].add(i);
+            int p = sc.nextInt();
+            if(p != -1){
+                tree[p].add(i);
+                tree[i].add(p);
+            }else{
+                root = i;
+            }
+
         }
-        M = sc.nextInt();
+        deleteNode = sc.nextInt();
 
-        DFS(0);
-
-        System.out.println(leafNode);
+        if(deleteNode != root){
+            DFS(root);
+            System.out.println(answer);
+        }else{
+            System.out.println(0);
+        }
 
     }
 
     private static void DFS(int parentNode){
-        if(parentNode == M) return;
+        isVisited[parentNode] = true;
 
         int childNodeCount=0;
         for(int childNode : tree[parentNode]){
-            if(childNode == M) continue;
+            if(childNode == deleteNode) continue;
             if(isVisited[childNode]) continue;
-            isVisited[childNode] = true;
 
             childNodeCount++;
             DFS(childNode);
         }
-        if(childNodeCount ==0) leafNode++;
+        if(childNodeCount ==0) answer++;
     }
 }
