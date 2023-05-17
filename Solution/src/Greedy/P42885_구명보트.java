ff013db8785ab3747ebadd36589e9c221615ae8f
heenamkang <abc_always@naver.com>
2023-05-17 15:37:49 +0900

    P42885_구명보트_4

--- Solution/src/Greedy/P42885_구명보트.java
+++ Solution/src/Greedy/P42885_구명보트.java
@@ -2,21 +2,52 @@ package Greedy;
 
 import java.util.Arrays;
 
-public class P42885_구명보트{
+//public class P42885_구명보트{
+//    public int solution(int[] people, int limit){
+//        Arrays.sort(people);
+//
+//        int count=0;
+//        int i=0, j=people.length-1;
+//        while(i<j){
+//            if(people[j]+people[i] <= limit){
+//                i++;
+//            }
+//            j--;
+//            count++;
+//        }
+//        if(i==j) count++;
+//
+//        return count;
+//    }
+//}
+
+import java.util.Arrays;
+        import java.util.Collections;
+
+class Solution{
     public int solution(int[] people, int limit){
         Arrays.sort(people);
-
         int count=0;
-        int i=0, j=people.length-1;
+        int i=0;
+        int j=people.length-1;
         while(i<j){
-            if(people[j]+people[i] <= limit){
+            if(people[i]+people[j]<=limit){
                 i++;
             }
             j--;
             count++;
         }
         if(i==j) count++;
-
         return count;
     }
 }
+
+public class P42885_구명보트{
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        int[] people = {70, 50, 80, 50};
+        int limit = 100;
+        int answer = s.solution(people, limit);
+    }
+}
+
