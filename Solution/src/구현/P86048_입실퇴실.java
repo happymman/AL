5d7724bfe75f0486d11eeb85c4e0b25f16d4ecec
heenamkang <abc_always@naver.com>
2023-05-18 19:39:11 +0900

    B2042_구간합구하기_2

--- Solution/src/구현/P86048_입실퇴실.java
+++ Solution/src/구현/P86048_입실퇴실.java
@@ -1,36 +1,36 @@
 package 구현;
-
-import java.util.*;
-
-public class P86048_입실퇴실 {
-    public int[] solution(int[] enter, int[] leave) {
-        int[] answer = new int[enter.length];
-        List<Integer>list = new ArrayList();
-        int idx=0;
-
-        //한명 들어올때마다
-        for(int i=0; i<answer.length;i++){
-            list.add(enter[i]);
-
-            //반드시 만난 사람 숫자 count
-            for(int j=0; j<list.size(); j++){
-                if(enter[i]==list.get(j)){ //방금 들어온 본인은
-                    answer[list.get(j)-1] = list.size()-1; //본인 제외 방에 있는 사람수만큼 마주쳤다.
-                }else{
-                    answer[list.get(j)-1]++; //원래 있던 사람들은 마주친사람이 한명 늘었다.
-                }
-            }
-
-            //내보낼 수 있는 조건이 충족되면 -> 최대한 빠르게 내보낸다. - 이유 : 그래야 만날 수도 있는 사람이 count안되고, 반드시 만날 수 밖에 없는 사람만 카운트 됨.
-            while(idx<leave.length && list.contains(leave[idx])){ //떠날 사람이 방에 있어야만 떠날까말까가 가능
-                list.remove(Integer.valueOf(leave[idx]));
-                idx++;
-            }
-        }
-
-        return answer;
-    }
-}
+//
+//import java.util.*;
+//
+//public class P86048_입실퇴실 {
+//    public int[] solution(int[] enter, int[] leave) {
+//        int[] answer = new int[enter.length];
+//        List<Integer>list = new ArrayList();
+//        int idx=0;
+//
+//        //한명 들어올때마다
+//        for(int i=0; i<answer.length;i++){
+//            list.add(enter[i]);
+//
+//            //반드시 만난 사람 숫자 count
+//            for(int j=0; j<list.size(); j++){
+//                if(enter[i]==list.get(j)){ //방금 들어온 본인은
+//                    answer[list.get(j)-1] = list.size()-1; //본인 제외 방에 있는 사람수만큼 마주쳤다.
+//                }else{
+//                    answer[list.get(j)-1]++; //원래 있던 사람들은 마주친사람이 한명 늘었다.
+//                }
+//            }
+//
+//            //내보낼 수 있는 조건이 충족되면 -> 최대한 빠르게 내보낸다. - 이유 : 그래야 만날 수도 있는 사람이 count안되고, 반드시 만날 수 밖에 없는 사람만 카운트 됨.
+//            while(idx<leave.length && list.contains(leave[idx])){ //떠날 사람이 방에 있어야만 떠날까말까가 가능
+//                list.remove(Integer.valueOf(leave[idx]));
+//                idx++;
+//            }
+//        }
+//
+//        return answer;
+//    }
+//}
 //
 //package 구현;
 //
@@ -64,3 +64,44 @@ public class P86048_입실퇴실 {
 //    }
 //}
 
+import org.w3c.dom.ls.LSOutput;
+
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] enter, int[] leave) {
+        List<Integer> room = new ArrayList<>();
+        int[] result = new int[enter.length];
+
+        int idx=0;
+        for(int i=0;i<enter.length;i++){
+            room.add(enter[i]);
+            for(int j=0;j<room.size();j++){
+                if(room.get(j) == enter[i]){
+                    result[enter[i]-1] += room.size()-1;
+                }else{
+                    result[room.get(j)-1]++;
+                }
+            }
+
+            while(!room.isEmpty() && room.contains(leave[idx])){
+                room.remove(Integer.valueOf(leave[idx]));
+                idx++;
+            }
+        }
+        return result;
+    }
+}
+
+public class P86048_입실퇴실 {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        int[] enter = {3,2,1};
+        int[] leave = {2,1,3};
+
+        int[] result = s.solution(enter, leave);
+        for(int i=0;i<result.length;i++){
+            System.out.println(result[i]);
+        }
+    }
+}
