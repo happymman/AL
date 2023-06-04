6f847cfef34a8197a90d5da3730eeb4f8968fa3b
heenamkang <abc_always@naver.com>
2023-06-04 15:25:47 +0900

    P86971_전력망둘로나누기_2

--- Solution/src/백트래킹/P86971_전력망둘로나누기.java
+++ Solution/src/백트래킹/P86971_전력망둘로나누기.java
@@ -56,3 +56,53 @@ class P86971_전력망둘로나누기Solution {
         }
     }
 }
+
+//2회 풀이
+//import java.util.*;
+//
+//public class Solution{
+//    static boolean[] isVisited;
+//    static ArrayList<Integer>[] tree;
+//
+//    public int solution(int n, int[][] wires){
+//        tree = new ArrayList[n+1];
+//
+//        for(int i=1;i<tree.length;i++){
+//            tree[i] = new ArrayList<>();
+//        }
+//
+//        for(int[] wire : wires){
+//            tree[wire[0]].add(wire[1]);
+//            tree[wire[1]].add(wire[0]);
+//        }
+//
+//        int min = Integer.MAX_VALUE;
+//        for(int[] wire : wires){
+//            isVisited = new boolean[n+1];
+//
+//            Set<Integer> wireNodes = new HashSet<>();
+//            wireNodes.add(wire[0]);
+//            wireNodes.add(wire[1]);
+//
+//            isVisited[1] = true;
+//            dfs(1, wireNodes);
+//
+//            int visitCount=0;
+//            for(int i=0;i<isVisited.length;i++){
+//                if(isVisited[i]) visitCount++;
+//            }
+//            int diff  = Math.abs((n-visitCount)-visitCount);
+//            if(min>diff) min = diff;
+//        }
+//        return min;
+//    }
+//
+//    static void dfs(int v1, Set<Integer> wireNodes){
+//        for(int v2 : tree[v1]){
+//            if(isVisited[v2]) continue;
+//            if(wireNodes.contains(v1)&& wireNodes.contains(v2)) continue;
+//            isVisited[v2] = true;
+//            dfs(v2, wireNodes);
+//        }
+//    }
+//}
