6123aa462721ce5b538854fff895053045b0a43c
heenamkang <abc_always@naver.com>
2023-06-06 16:12:13 +0900

    P84512_모음사전_2

--- Solution/src/완전탐색/P84512_모음사전.java
+++ Solution/src/완전탐색/P84512_모음사전.java
@@ -14,3 +14,36 @@ public class P84512_모음사전 {
         return order;
     }
 }
+
+// By DFS
+//import java.util.*;
+//
+//class Solution {
+//
+//    static char[] arr;
+//    static List<String> wordList = new ArrayList<>();
+//
+//    public int solution(String word) {
+//
+//        arr = new char[]{'A', 'E', 'I', 'O', 'U'};
+//
+//        dfs("",0);
+//
+//        for(int i=0;i<wordList.size();i++){
+//            if(wordList.get(i).equals(word)) return i;
+//        }
+//        return -1;
+//    }
+//
+//    static void dfs(String str, int count){
+//        wordList.add(str);
+//
+//        if(count==5){
+//            return;
+//        }
+//
+//        for(int i=0;i<5;i++){
+//            dfs(str+arr[i], count+1);
+//        }
+//    }
+//}
