3c529b3caf437d7c3118af96f7d27b3211332f7e
heenamkang <abc_always@naver.com>
2023-07-27 20:10:21 +0900

    P12909_올바른괄호2

--- Solution/src/문자열/P60057_문자열압축.java
+++ Solution/src/문자열/P60057_문자열압축.java
@@ -4,8 +4,12 @@ public class P60057_문자열압축 {
     public static void main(String[] args) {
         P60057_문자열압축Solution sl = new P60057_문자열압축Solution();
 
-        String s = 	"abcabcabcabcdededededede";
-        System.out.println(sl.solution(s));
+        String s1 = "aabbaccc"; //7
+        String s2 = "ababcdcdababcdcd"; //9
+        String s3 = "abcabcdede"; //8
+        String s4 = "abcabcabcabcdededededede"; //14
+        String s5 = "xababcdcdababcdcd"; //17
+        System.out.println(sl.solution(s5));
     }
 }
 
@@ -51,6 +55,44 @@ class Solution {
 }
 */
 
+//class P60057_문자열압축Solution {
+//    public int solution(String s) {
+//        int answer = s.length();
+//
+//        for(int i=1 ; i<=s.length()/2 ; i++){ //3
+//            int rCount = 1;
+//            String stdStr = s.substring(0,i);
+//            StringBuilder result = new StringBuilder();
+//
+//            for(int j=i ; j<=s.length() ; j+=i){
+//                String next = s.substring(j,j+i<s.length() ? j+1 : s.length()); //
+//                if(stdStr.equals(next)){
+//                    rCount++;
+//                }else{
+//                    result.append((rCount==1?"":rCount)+stdStr); //2a2b4a
+//                    rCount = 1;
+//                    stdStr = next;
+//                }
+//            }
+//            /*
+//                i)이전for문의 j+i가 s.length()인경우,
+//                다음for문의 j는 s.length()가 되어 for문을 벗어나지않게되고
+//                -> s.substring(s.length(), s.length())
+//                -> ""공백을 잘라내어 else문을 실행(&stdStr = ""가되어 for문 벗어난뒤 result에 추가해도 영향없음)
+//
+//                ii)이전for문의 j+i가 s.length()보다 더큰경우,
+//                다음for문의 j는 s.length()를 초과하게되어 for문을 벗어나게되고
+//                -> stdStr은 append되지 않은채 남아있으므로
+//                여기서 append해준다.
+//             */
+//            result.append(stdStr);
+//            answer = Math.min(answer, result.length());
+//        }
+//
+//        return answer;
+//    }
+//}
+
 class P60057_문자열압축Solution {
     public int solution(String s) {
         int answer = s.length();
@@ -60,8 +102,8 @@ class P60057_문자열압축Solution {
             String stdStr = s.substring(0,i);
             StringBuilder result = new StringBuilder();
 
-            for(int j=i ; j<=s.length() ; j+=i){
-                String next = s.substring(j,j+i<s.length() ? j+1 : s.length()); //
+            for(int j=i ; j<s.length() ; j+=i){
+                String next = s.substring(j,j+i<s.length() ? j+i : s.length()); //
                 if(stdStr.equals(next)){
                     rCount++;
                 }else{
@@ -70,21 +112,9 @@ class P60057_문자열압축Solution {
                     stdStr = next;
                 }
             }
-            /*
-                i)이전for문의 j+i가 s.length()인경우,
-                다음for문의 j는 s.length()가 되어 for문을 벗어나지않게되고
-                -> s.substring(s.length(), s.length())
-                -> ""공백을 잘라내어 else문을 실행(&stdStr = ""가되어 for문 벗어난뒤 result에 추가해도 영향없음)
-
-                ii)이전for문의 j+i가 s.length()보다 더큰경우,
-                다음for문의 j는 s.length()를 초과하게되어 for문을 벗어나게되고
-                -> stdStr은 append되지 않은채 남아있으므로
-                여기서 append해준다.
-             */
-            result.append(stdStr);
+            result.append((rCount==1?"":rCount)+stdStr);
             answer = Math.min(answer, result.length());
         }
-
         return answer;
     }
 }
