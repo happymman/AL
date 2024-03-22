6051d0a5c3d39d48b084fdbe39c83d89253d247d
happyman <37496620+happymman@users.noreply.github.com>
2024-03-22 20:43:18 +0900

    [level 1] Title: 성격 유형 검사하기, Time: 0.41 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/118666. 성격 유형 검사하기/성격 유형 검사하기.py
@@ -0,0 +1,41 @@
+"""
+    813
+    -829
+"""
+import math
+from collections import defaultdict
+
+def solution(surveys, choices):
+    
+    character = defaultdict(int)
+    
+    for i in range(len(surveys)) :#설문 선택
+        survey = surveys[i]
+        a,b = survey
+        
+        #점수 추가
+        character[a if choices[i] < 4 else b] += abs(4-choices[i])
+             
+    answer=[]
+    if character['T'] > character['R'] :
+        answer.append('T')
+    else : 
+        answer.append('R')
+        
+    if character['F'] > character['C'] :
+        answer.append('F')
+    else : 
+        answer.append('C')
+        
+    if character['M'] > character['J'] :
+        answer.append('M')
+    else : 
+        answer.append('J')
+        
+    if character['N'] > character['A'] :
+        answer.append('N')
+    else : 
+        answer.append('A')
+        
+    
+    return ''.join(answer)
