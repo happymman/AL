1ccb511f320115536af9cae7cb743375b3e1df95
happyman <37496620+happymman@users.noreply.github.com>
2024-03-22 20:05:57 +0900

    Update 주차 요금 계산.py

--- 프로그래머스/2/92341. 주차 요금 계산/주차 요금 계산.py
+++ 프로그래머스/2/92341. 주차 요금 계산/주차 요금 계산.py
@@ -9,7 +9,7 @@ def calculate(fees, fee) : #요금계산 ex : [180, 5000, 10, 600], 334
 
     sum = fees[1] #기본요금
     if fee > fees[0] : #기본시간 초과했을시, 
-        sum += int(math.ceil((fee - fees[0])/fees[2])*fees[3])
+        sum += int((math.ceil((fee - fees[0])/fees[2])*fees[3]))
     return sum
     
 def convert(time) : #시간변환 ex : "09:49"
@@ -28,17 +28,11 @@ def solution(fees, records):
         if inOut == 'IN' :
             carIn[carNum] = convert(time) #carIn에 등록
         else :
-            # print(str(convert(time) - carIn[carNum]))
             carTime[carNum] += convert(time) - carIn[carNum]
             del carIn[carNum]
-            
     
     for carNum in carIn.keys() : #안나간차있으면 23:59출차로해서 등록
         carTime[carNum] += convert("23:59") - carIn[carNum]
-        # del carIn[carNum]
-        
-    # for k, v in carTime.items() :
-    #     print("carNum : "+str(k)+" "+"carTime : "+str(v))
         
     for carNum in sorted(carTime.keys()) : #요금계산
         answer.append(calculate(fees, carTime[carNum]))
