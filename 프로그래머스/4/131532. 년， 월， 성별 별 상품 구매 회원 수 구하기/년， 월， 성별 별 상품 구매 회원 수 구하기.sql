6c1cc6f981d541d0cfe7d124d2f03f23924b4da5
happyman <37496620+happymman@users.noreply.github.com>
2024-03-27 16:34:17 +0900

    [level 4] Title: 년, 월, 성별 별 상품 구매 회원 수 구하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/4/131532. 년， 월， 성별 별 상품 구매 회원 수 구하기/년， 월， 성별 별 상품 구매 회원 수 구하기.sql
@@ -0,0 +1,44 @@
+/**/
+# SELECT extract(year from sales_date) as year, extract(month from sales_date) as month, gender, count(distinct u.user_id) as users
+# from user_info u
+# join online_sale o on u.user_id = o.user_id
+# where u.gender is not null
+# group by extract(year from o.sales_date), extract(month from o.sales_date),u.gender
+# order by year, month,u.gender;
+
+/**/
+/**/
+/**/
+/**/
+/**/
+/**/
+/**/
+/**/
+# /**/
+# /**/
+# select extract(year from sales_date) year, extract(month from sales_date) month, u.gender, count(distinct u.user_id) as users
+# from user_info u
+# join online_sale s on u.user_id = s.user_id
+# where u.gender is not null
+# group by extract(year from sales_date), extract(month from sales_date), u.gender
+# order by year, month, u.gender;
+
+
+select extract(year from o.sales_date) year, extract(month from o.sales_date) month, gender, count(distinct u.user_id) users
+from user_info u
+join online_sale o on u.user_id = o.user_id
+where u.gender is not null
+group by extract(year from o.sales_date)
+    ,extract(month from o.sales_date)
+    ,gender
+order by year, month, gender
+    
+
+
+
+
+
+
+
+
+
