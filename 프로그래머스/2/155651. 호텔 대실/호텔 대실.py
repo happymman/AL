d53fc2a89dd8038c4bd8eb5331cf74c13c47dbde
happyman <37496620+happymman@users.noreply.github.com>
2024-04-04 20:54:00 +0900

    Update 호텔 대실.py

--- 프로그래머스/2/155651. 호텔 대실/호텔 대실.py
+++ 프로그래머스/2/155651. 호텔 대실/호텔 대실.py
@@ -1,15 +1,13 @@
 """
-    704~711, 719~731
-    n<= 1000
-    
-    이걸 어떻게 하지?
-    1개 필요하다가 2개 필요 
-    
-    735 뒤늦게 투포인터 풀이여부를 판단했다.
-        판단 기준이 한개만 있으면, 가능한거 같은데
-        입실 퇴실시간 기준이 두개여서 안되는 것 같다.
-        FIFO가 아니여서. 
-    
+    소요시간 : 50m 
+    조건 : n<= 1000
+
+    피드백
+    1.우선순위큐 - 원소 : [리스트] -> 일때 전부 안넣어도 됨. ex : 퇴실시간만 넣어도됨
+    2.코드 - All -> 코드그림 보기/그리기(특히, 배열[인덱스] 코드 작성할때, 확인해줘야한다. 실수 자주 발생)
+    3.투포인터 - O상황 : FIFO, 우선순위FO(1개의 기준)
+        포인터 간격을 방개수라고 가정했을때, 일관된 기준에의해서 포인터 이동이 불가
+            이유 : 기준2개(입실시간, 퇴실시간)
 """
 import heapq
 def convert(time) :
@@ -23,31 +21,21 @@ def solution(book_time):
     #대기열 시간변환(입실시간-> 그대로, 퇴실시간-> +10)
     book_time = list(map(convert, book_time))
         
-    #대기열 정렬(입실시간 기준(거꾸로) - pop하기 위해서))
+    #대기열 정렬(입실시간 기준(거꾸로) - pop for O(1))
     book_time.sort(key = lambda x:x[0], reverse = True)
     
-    pq = [] #방 우선순위큐 선언(퇴실시간 기준)
-    
-    print('book_time : '+str(book_time))
-    print()
+    pq = [] # pq : 호텔
     
     maxV = 0
-    # cnt = 0
     while book_time : #대기열 안비었을때
-        time = book_time[-1][0] #시간 업데이트 by 그다음 빠른 입실시간(of 대기열)
+        time = book_time[-1][0] #시간 업데이트(by 다음 입실시간)(of 대기열)
         
-        while pq and pq[0] <= time : #방 우선순위큐 안비었을때 & 맨앞 뺄수 있다(퇴실시간 <= 현재시간)
-             heapq.heappop(pq) #방 우선순위큐에서 뺄수 있는애들 뺀다
+        while pq and pq[0] <= time : #퇴실 가능
+             heapq.heappop(pq) #퇴실(from 호텔)
         
-        while book_time and book_time[-1][0] <= time : #대기열 안비었을때 & 맨뒤 넣을 수 있다: 
-            person = book_time.pop()
-            heapq.heappush(pq, person[1]) #한걸 heappush()로 계속 넣기
+        while book_time and book_time[-1][0] <= time : #입실 가능
+            heapq.heappush(pq, book_time.pop()[1]) #입실(from 대기열)
         
-        maxV = max(maxV, len(pq)) #max방개수 갱신
-        
-        # cnt +=1
-        # print(str(time)+' '+str(maxV)+' '+ str(cnt))
-        # print('대기열 : '+str(book_time))
-        # print('방 : '+str(pq))
+        maxV = max(maxV, len(pq)) #방개수 최댓값 업데이트
     
-    return maxV #max방개수
+    return maxV
