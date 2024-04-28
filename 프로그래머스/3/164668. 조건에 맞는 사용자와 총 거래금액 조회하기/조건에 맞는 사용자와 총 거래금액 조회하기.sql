3a0c05b215ad8c5010657897cf724d346a09e186
happyman <37496620+happymman@users.noreply.github.com>
2024-04-28 10:44:01 +0900

    [level 3] Title: 조건에 맞는 사용자와 총 거래금액 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/164668. 조건에 맞는 사용자와 총 거래금액 조회하기/조건에 맞는 사용자와 총 거래금액 조회하기.sql
+++ 프로그래머스/3/164668. 조건에 맞는 사용자와 총 거래금액 조회하기/조건에 맞는 사용자와 총 거래금액 조회하기.sql
@@ -1,21 +1,10 @@
-
-select u.user_id, u.nickname, sum(b.price) as total_sales
-from used_goods_board b
-join used_goods_user u on b.writer_id = u.user_id
-where b.status = 'DONE'
-group by u.user_id
-having total_sales >= 700000
-order by total_sales asc
-
-
-
-
-
-
-
-
-
-
+# select u.user_id, u.nickname, sum(b.price) as total_sales
+# from used_goods_board b
+# join used_goods_user u on b.writer_id = u.user_id
+# where b.status = 'DONE'
+# group by u.user_id
+# having total_sales >= 700000
+# order by total_sales asc
 
 
 
@@ -27,3 +16,20 @@ order by total_sales asc
 # group by b.writer_id
 # having total_sales >= 700000
 # order by total_sales asc;
+
+
+select u.user_id, u.nickname, sum(b.price) as total_sales
+from used_goods_board b
+join used_goods_user u on b.writer_id = u.user_id
+where b.status = "DONE"
+group by b.writer_id
+having sum(b.price) >= 700000
+order by sum(b.price)
+
+
+
+
+
+
+
+
