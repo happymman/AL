a8f1d12cffeefd362fd450443ebbf74f4327b6ac
happyman <37496620+happymman@users.noreply.github.com>
2025-04-30 22:58:04 +0900

    [Gold IV] Title: 전구와 스위치, Time: 96 ms, Memory: 34760 KB -BaekjoonHub

--- 백준/Gold/2138. 전구와 스위치/전구와 스위치.py
+++ 백준/Gold/2138. 전구와 스위치/전구와 스위치.py
@@ -1,33 +1,33 @@
-n = int(input())
-bulb = list(map(int, input()))
-target = list(map(int, input()))
 
+def greedy(start, tgt) :
+    start_copy = start[:]
 
-def change(bulb, target):
-    bulb_copy = bulb[:]
-    press = 0
+    cnt=0 #cnt : 스위치 횟수
+    for i in range(1,n) :
+        if start_copy[i-1] == tgt[i-1] : continue #직전 전구 같으면 pass
 
-    for i in range(1, n):
-        # 전체 비교x, 직전만 비교o(같다면 스위치 누르지 않음)
-        if bulb_copy[i-1] == target[i-1] : continue
-        
-        press += 1
-        #전구 반전
+        #반전
+        cnt += 1
         for j in range(i-1, i+2) :
-            if j<n : bulb_copy[j] = 1 - bulb_copy[j]
+            if j<n : start_copy[j] = 1-start_copy[j]
 
-    if bulb_copy == target : return press 
+    if start_copy == tgt : return cnt
     else : return 1e9
 
-answer = change(bulb, target)
 
-# 첫번째 전구스위치 누르기
-bulb[0] = 1 - bulb[0]
-bulb[1] = 1 - bulb[1]
 
-#비교
-answer = min(answer, change(bulb, target) + 1)# min(첫번째 전구x, 첫번째 전구 o)
-if answer != 1e9:
-    print(answer)
-else:
-    print(-1)
+n = int(input())
+
+tgt = list(map(int, input()))
+start = list(map(int, input()))
+
+ans = greedy(start, tgt)
+
+#첫번째 스위치 누르고 시작
+start[0] = 1 - start[0]
+start[1] = 1 - start[1]
+
+ans = min(ans, greedy(start, tgt)+1)
+
+if ans != 1e9 : print(ans)
+else : print(-1)
