1646808ffca272d97fa0126bd0cbff855743af3d
happyman <37496620+happymman@users.noreply.github.com>
2025-09-26 21:49:06 +0900

    [level 4] Title: 저자 별 카테고리 별 매출액 집계하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/4/144856. 저자 별 카테고리 별 매출액 집계하기/저자 별 카테고리 별 매출액 집계하기.sql
+++ 프로그래머스/4/144856. 저자 별 카테고리 별 매출액 집계하기/저자 별 카테고리 별 매출액 집계하기.sql
@@ -1,33 +1,43 @@
-#문법 실수 - join할대 =대신 and
-#추출 문법 - extract(year from 컬럼)
+# #문법 실수 - join할대 =대신 and
+# #추출 문법 - extract(year from 컬럼)
 
-# select a.author_id, a.author_name, b.category, sum(bs.sales * b.price) as total_sales
+# # select a.author_id, a.author_name, b.category, sum(bs.sales * b.price) as total_sales
+# # from book b
+# # join author a on b.author_id = a.author_id
+# # join book_sales bs on b.book_id = bs.book_id
+# # where extract(year from bs.sales_date) = 2022
+# #     and extract(month from bs.sales_date) = 1
+# # group by a.author_id, b.category
+# # order by a.author_id asc, b.category desc
+
+
+# # select a.author_id, a.author_name, b.category, sum(b.price * bs.sales) as total_sales
+# # from book b
+# # join author a on b.author_id = a.author_id
+# # join book_sales bs on b.book_id = bs.book_id
+# # where extract(year from bs.sales_date) = 2022
+# #     and extract(month from bs.sales_date) = 1
+# # group by a.author_id, b.category
+# # order by a.author_id, b.category desc
+
+# select a.author_id, a.author_name, b.category, (sales * price) as total_sales
 # from book b
 # join author a on b.author_id = a.author_id
-# join book_sales bs on b.book_id = bs.book_id
-# where extract(year from bs.sales_date) = 2022
-#     and extract(month from bs.sales_date) = 1
-# group by a.author_id, b.category
-# order by a.author_id asc, b.category desc
-
-
-# select a.author_id, a.author_name, b.category, sum(b.price * bs.sales) as total_sales
-# from book b
-# join author a on b.author_id = a.author_id
-# join book_sales bs on b.book_id = bs.book_id
-# where extract(year from bs.sales_date) = 2022
-#     and extract(month from bs.sales_date) = 1
+# join book_sales bs on b.book_id = bs.book_id 
+# where extract (year from bs.sales_date)=2022
+#     and extract (month from bs.sales_date)=1
 # group by a.author_id, b.category
 # order by a.author_id, b.category desc
 
-select a.author_id, a.author_name, b.category, sum(sales * price) as total_sales
+select a.author_id, a.author_name, b.category, sum(b.price*bs.sales) as total_sales
 from book b
 join author a on b.author_id = a.author_id
-join book_sales bs on b.book_id = bs.book_id 
-where extract (year from bs.sales_date)=2022
-    and extract (month from bs.sales_date)=1
+join book_sales bs on b.book_id = bs.book_id
+where extract (year from bs.sales_date) = 2022 
+    and extract(month from bs.sales_date) = 1
 group by a.author_id, b.category
 order by a.author_id, b.category desc
+
 
 
 
