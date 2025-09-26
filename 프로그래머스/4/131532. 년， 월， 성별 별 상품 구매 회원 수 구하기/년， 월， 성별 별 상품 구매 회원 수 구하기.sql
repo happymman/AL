ec0af04b122acee314acd8837a2368d811efa549
happyman <37496620+happymman@users.noreply.github.com>
2025-09-26 23:21:13 +0900

    [level 4] Title: 년, 월, 성별 별 상품 구매 회원 수 구하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/4/131532. 년， 월， 성별 별 상품 구매 회원 수 구하기/년， 월， 성별 별 상품 구매 회원 수 구하기.sql
+++ 프로그래머스/4/131532. 년， 월， 성별 별 상품 구매 회원 수 구하기/년， 월， 성별 별 상품 구매 회원 수 구하기.sql
@@ -8,12 +8,6 @@
 
 /**/
 /**/
-/**/
-/**/
-/**/
-/**/
-/**/
-/**/
 # /**/
 # /**/
 # select extract(year from sales_date) year, extract(month from sales_date) month, u.gender, count(distinct u.user_id) as users
@@ -33,24 +27,47 @@
 #     ,gender
 # order by year, month, gender
 
+# select extract(year from o.sales_date) as year,
+#     extract(month from o.sales_date) as month,
+#     u.gender as gender,
+#     count(distinct u.user_id) as users 
+# from user_info u
+# join online_sale o on u.user_id = o.user_id
+# where u.gender is not null
+# group by extract(year from o.sales_date),
+#     extract(month from o.sales_date),
+#     u.gender
+# order by extract(year from o.sales_date),
+#     extract(month from o.sales_date),
+#     u.gender
+
+
 select extract(year from o.sales_date) as year,
-    extract(month from o.sales_date) as month,
-    u.gender as gender,
-    count(distinct u.user_id) as users 
+    extract (month from o.sales_date) as month,
+    u.gender,
+    count(distinct(u.user_id)) as user
 from user_info u
-join online_sale o on u.user_id = o.user_id
+join online_sale o on u.user_id=o.user_id
 where u.gender is not null
-group by extract(year from o.sales_date),
-    extract(month from o.sales_date),
-    u.gender
-order by year, month, gender
+group by extract (year from o.sales_date),extract (month from o.sales_date),u.gender
+order by extract (year from o.sales_date),extract (month from o.sales_date),u.gender
 
-
-
-
-
-
-
-
-
-
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
